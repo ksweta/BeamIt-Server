bd725aaae4a7609021015e48581b61145d80a6ec
kumari.sweta <ksweta1007@gmail.com>
2015-11-01 09:51:13 -0800

    Encrypted password field

--- beamit/model/user.py
+++ beamit/model/user.py
@@ -1,3 +1,7 @@
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash
+)
 from beamit.app import db
 from beamit.resources.user import User as UserResource
 
@@ -5,7 +9,7 @@ from beamit.resources.user import User as UserResource
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(160), nullable=False)
     name = db.Column(db.String(64))
     phone = db.Column(db.String(15))
     company = db.Column(db.String(50))
@@ -15,7 +19,8 @@ class User(db.Model):
 
     def __init__(self, email, password,  name=None, phone=None, company=None, linkedin_url=None, photo=None):  # noqa
         self.email = email
-        self.password = password
+        # Generate the one way hash password
+        self.set_password(password)
         self.name = name
         self.phone = phone
         self.company = company
@@ -34,6 +39,19 @@ class User(db.Model):
                 self.linkedin_url,
                 True if self.photo else False,
             )
+
+    def set_password(self, password):
+        """
+        This method generates one way hash of the password
+        """
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        """
+        This method generates one way hash of the given password and compares it with the stored
+        password
+        """
+        return check_password_hash(self.password, password)
 
     def to_user_resource(self):
         return UserResource(
